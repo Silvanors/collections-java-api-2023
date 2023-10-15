acf5e7c79918bbef184f6f599f780732ccb6e1b4
Silvano <silvanors@gmail.com>
2023-10-15 16:57:26 -0300

    Subindo collections Set

---
+++ src/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,59 @@
+package set.Ordenacao;
+
+import set.Ordenacao.Produto.ComparatorPorPreco;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+    //atributo
+    private Set<Produto> produtoSet;
+
+    //construtor
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long codigo, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(nome, codigo, quantidade, preco));
+    }
+
+    public Set<Produto> exibirProdutosPorNome(){
+        //HashSet<>() não permite organizar, o TreeSer<>() permite e não precisa utilizar o Collection.sort()
+        //o TreeSet<>() consegue organizar
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+
+    public Set<Produto> exibirProdutosPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        System.out.println(cadastroProdutos.produtoSet);
+
+        // Adicionando produtos ao cadastro
+        cadastroProdutos.adicionarProduto(1L, "Smartphone", 1000d, 10);
+        cadastroProdutos.adicionarProduto(2L, "Notebook", 1500d, 5);
+        cadastroProdutos.adicionarProduto(1L, "Mouse", 30d, 20);
+        cadastroProdutos.adicionarProduto(4L, "Teclado", 50d, 15);
+
+        //Lista após adicionar produtos
+        System.out.println(cadastroProdutos.produtoSet);
+
+        // Exibindo produtos ordenados por nome
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+        // Exibindo produtos ordenados por preço
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
+
+
+}
