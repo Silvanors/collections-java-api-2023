b98e67433087457c00b119fe9e75fb9b498e910a
Silvanors <silvanors@gmail.com>
2023-10-16 11:51:32 -0300

    Adicionando Collections List

---
+++ src/list/Ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,66 @@
+package list.Ordenacao;
+
+import java.security.cert.CollectionCertStoreParameters;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome,idade,altura));
+    }
+
+    //ordenar as pessoas usando o Comparable (há necessidade de ir na Classe Pessoa e implementar a interface
+    // Comparable!!!)
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        //Collections consegue ordenar pelo método Comparable implementado na Classe Pessoa.
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        // Criando uma instância da classe OrdenacaoPessoas
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+
+        /*// Adicionando pessoas à lista
+        ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
+        ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
+        ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);*/
+
+        // Adicionando pessoas à lista
+        ordenacaoPessoas.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacaoPessoas.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoas.adicionarPessoa("Nome 4", 17, 1.56);
+
+
+
+        // Exibindo a lista de pessoas adicionadas
+        System.out.println(ordenacaoPessoas.pessoaList);
+
+        // Ordenando e exibindo por idade
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+
+        // Ordenando e exibindo por altura
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    }
+
+
+
+}
---
+++ src/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,86 @@
+package list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    //atributo
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        livroList.add(new Livro(titulo, autor,anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for(Livro l: livroList) {
+                if(l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+
+        }else {
+            System.out.println("A lista está vazia!");
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for(Livro l: livroList) {
+                if(l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+
+        }else {
+            System.out.println("A lista está vazia!");
+        }
+        return livrosPorIntervaloAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if(!livroList.isEmpty()){
+            for(Livro l: livroList){
+                if(l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+        return livroPorTitulo;
+    }
+
+    public static void main(String[] args) {
+
+        // Criando uma instância do catálogo de livros
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        // Adicionando livros ao catálogo
+        /*catalogoLivros.adicionarLivro("Microsserviços Prontos Para a Produção", "Susan J. Fowler", 2017);
+        catalogoLivros.adicionarLivro("Java Guia do Programador", "Peter Jandl Junior", 2021);
+        catalogoLivros.adicionarLivro("Código Limpo", "Robert C. Martin", 2009);
+        catalogoLivros.adicionarLivro("O Codificador Limpo", "Robert C. Martin", 2012);*/
+
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2021);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2017);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 2", 2009);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 2012);
+
+        // Exibindo livros pelo mesmo autor
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2008,2010));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+    }
+
+}
